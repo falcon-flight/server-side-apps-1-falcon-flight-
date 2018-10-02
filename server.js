97e28c52918d5c0a0b51aa340f6f0fb25907662b
Walter <walter.lindop@yale.edu>
2018-10-02 03:13:46 +0000

    Adjusting error in server.js code

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.send('Hello MGT-656/660!');
+app.get('/nickname', (request, response) => {
+    response.send('falcon-flight');
 });
 
 app.listen(process.env.PORT || 4000);
