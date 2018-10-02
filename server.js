c98fe5e60d6ed6c005c846400e8a6ad4c0e1f32f
Walter <walter.lindop@yale.edu>
2018-10-02 03:09:55 +0000

    Revising server.js file to test as getting an error on Heroku

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.send('Hello MGT-656/660!');
-app.get('/nickname', (request, response) => {
-    response.send('falcon-flight');
 });
 
 app.listen(process.env.PORT || 4000);
