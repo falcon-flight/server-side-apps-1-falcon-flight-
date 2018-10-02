5111ea3bbe1d2acde5b2b5957a85ea1d3ab8e8fe
Walter <walter.lindop@yale.edu>
2018-10-02 03:16:03 +0000

    Adding brackets in server.js code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.get('/', (request, response) => {
     response.send('Hello MGT-656/660!');
+});   
 app.get('/nickname', (request, response) => {
     response.send('falcon-flight');
 });
